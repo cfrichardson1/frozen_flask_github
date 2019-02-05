6be690511c4d7d80babdbf1ffad13cf8056907ca
cfrichardson1 <crichardsonsjsu@gmail.com>
2019-02-04 21:52:41 -0800

    foundation of rebuild set with example page built in all-in-one HTML

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app
 from flask import redirect, render_template, url_for
 
 
-@app.route('/index.html')
+@app.route('/')
 def home():
 	return render_template('homepage.html', title_bar ='Homepage')
 
