39368f36f7527090c1703cd871e2a45596d6b1d2
cfrichardson1 <crichardsonsjsu@gmail.com>
2019-02-21 10:56:50 -0800

    base update, needs further updating based on Jenna's recommendations

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,4 @@ from flask import redirect, render_template, url_for
 
 @app.route('/')
 def home():
-	return render_template('homepage_exp.html', title_bar ='Homepage')
+	return render_template('homepage.html', title_bar ='Homepage')
