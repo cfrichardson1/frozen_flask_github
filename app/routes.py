cc04477fd2abd52015ef9fae04d63ebd97a9a122
cfrichardson1 <crichardsonsjsu@gmail.com>
2019-03-02 08:50:04 -0800

    retemplating base

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
+# need to transfer all apps into this one app
 from app import app
 from flask import redirect, render_template, url_for
 
 
-@app.route('/')
+@app.route('/index.html')
 def home():
 	return render_template('homepage.html', title_bar ='Homepage')
+
+@app.route('/PyCitySchools_EDA.html')
+def PyCity():
+	return render_template('PySchool_Report_colab0.html', title_bar ='Test')
