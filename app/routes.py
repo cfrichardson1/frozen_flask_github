0ff24fbabad03b25ace5d4897b94197198174841
Chris Richardson <cfrichardson1@gmail.com>
2020-06-02 21:26:28 -0700

    needs to be rebuilt for mobile 1st

--- app/routes.py
+++ app/routes.py
@@ -5,8 +5,8 @@ from flask import redirect, render_template, url_for
 
 @app.route('/index.html')
 def home():
-	return render_template('homepage.html', title_bar ='Homepage')
+	return render_template('site_homepage.html', title_bar ='Homepage')
 
 @app.route('/PyCitySchools_EDA.html')
 def PyCity():
-	return render_template('PySchool_Report_colab0.html', title_bar ='Test')
+	return render_template('PySchool_Report_colab.html', title_bar ='Test')
